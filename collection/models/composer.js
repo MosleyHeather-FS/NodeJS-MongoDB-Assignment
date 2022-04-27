57fb06e23878558ef6cd61ddafd8e507802876b8
Heather Mosley <hmosley@student.fullsail.edu>
2022-04-27 03:59:31 -0500

    Changed id to type which fixed errors

--- collection/models/composer.js
+++ collection/models/composer.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const composerSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  Composition: {
-    id: mongoose.Schema.Types.ObjectId,
+  composition: {
+    type: mongoose.Schema.Types.ObjectId,
     ref: "Composition",
     required: true,
   },
